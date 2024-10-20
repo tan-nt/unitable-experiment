497a47ef2c24af10729ddc72d43cbac2f811c38c
Tan Huynh <tan.huynh@anfin.vn>
2024-10-21 02:44:13 +0700

    update

--- CONFIG.mk
+++ CONFIG.mk
@@ -113,17 +113,17 @@ SEQ512 = trainer.max_seq_len=512
 SEQ1024 = trainer.max_seq_len=1024
 
 # batch size + epoch
-BATCH24 = ++trainer.train.dataloader.batch_size=24 ++trainer.valid.dataloader.batch_size=24
-BATCH48 = ++trainer.train.dataloader.batch_size=48 ++trainer.valid.dataloader.batch_size=48
-BATCH72 = ++trainer.train.dataloader.batch_size=72 ++trainer.valid.dataloader.batch_size=72
-BATCH80 = ++trainer.train.dataloader.batch_size=80 ++trainer.valid.dataloader.batch_size=80
-BATCH96 = ++trainer.train.dataloader.batch_size=96 ++trainer.valid.dataloader.batch_size=96
-BATCH256 = ++trainer.train.dataloader.batch_size=256 ++trainer.valid.dataloader.batch_size=256
-BATCH384 = ++trainer.train.dataloader.batch_size=384 ++trainer.valid.dataloader.batch_size=384
+BATCH24 = ++trainer.train.dataloader.batch_size=24 ++trainer.valid.dataloader.batch_size=24 ++trainer.test.dataloader.batch_size=24
+BATCH48 = ++trainer.train.dataloader.batch_size=48 ++trainer.valid.dataloader.batch_size=48 ++trainer.test.dataloader.batch_size=48
+BATCH72 = ++trainer.train.dataloader.batch_size=72 ++trainer.valid.dataloader.batch_size=72 ++trainer.test.dataloader.batch_size=72
+BATCH80 = ++trainer.train.dataloader.batch_size=80 ++trainer.valid.dataloader.batch_size=80 ++trainer.test.dataloader.batch_size=80
+BATCH96 = ++trainer.train.dataloader.batch_size=96 ++trainer.valid.dataloader.batch_size=96 ++trainer.test.dataloader.batch_size=96
+BATCH256 = ++trainer.train.dataloader.batch_size=256 ++trainer.valid.dataloader.batch_size=256 ++trainer.test.dataloader.batch_size=256
+BATCH384 = ++trainer.train.dataloader.batch_size=384 ++trainer.valid.dataloader.batch_size=384 ++trainer.test.dataloader.batch_size=384
 
-EPOCH24 = ++trainer.train.epochs=24
-EPOCH30 = ++trainer.train.epochs=30
-EPOCH48 = ++trainer.train.epochs=48
+EPOCH24 = ++trainer.train.epochs=24 ++trainer.valid.epochs=24 ++trainer.test.epochs=24
+EPOCH30 = ++trainer.train.epochs=30 ++trainer.valid.epochs=30 ++trainer.test.epochs=30
+EPOCH48 = ++trainer.train.epochs=48 ++trainer.valid.epochs=48 ++trainer.test.epochs=48
 
 # optimizer
 OPT_ADAMW = trainer/train/optimizer=adamw
